8f8fbadc8f9f34afcbf06fe69b89965b0e0a6640
jungai <juworaphol125@gmail.com>
2021-09-05 17:55:08 +0700

    follow style guide

--- lib/cron/cron.ex
+++
@@ -1,9 +0,0 @@
-defmodule NonnMonn.Scheduler do
-  use Quantum, otp_app: :nonn_monn
-end
-
-defmodule NonnMonn.Task do
-  def hello_world() do
-    IO.puts("hello Sched")
-  end
-end
--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule NonnMonn.MixProject do
   def application do
     [
       extra_applications: [:logger],
-      mod: {NonnMonn.Application, []}
+      mod: {NonnMonn, []}
     ]
   end
 
